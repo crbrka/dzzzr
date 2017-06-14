529232b27d6de2d3554d1d512a7895bb86181840
a.tuhvatullin <almir@tuhvatull.in>
2017-06-14 12:17:06 +0300

    auth model for teams ready for use

--- olympic/views.py
+++ olympic/views.py
@@ -1,25 +1,50 @@
 from django.shortcuts import render
-#from django.contrib import sessions
+from django.http import HttpResponseRedirect
+
 from . import urls
 from .forms import *
 from .models import *
 
-
-# Create your views here.
-
+auth_page = 'olympic/auth.html'
+logged_page = 'olympic/main.html'
 
 def index(request):
-    is_user = request.session.get('is_valid')
-    if not is_user == 'True':
-        print(request.POST)
-        print('HELLOE')
-        u = Teams.objects.get(login=request.POST.get('username'))
-        if u.password == request.POST['password']:
-            request.session['member_id'] = m.id
-            request.session['is_valid'] = True;
-        return render(request, 'olympic/auth.html', locals())
+
+    if request.session.get('is_logged', False):
+        return render(request, logged_page, locals())
     else:
-        print(request.session.get('is_valid'))
-        print('BBB')
-        return render(request, 'olympic/main.html', locals())
+        if request.session.get('404', False):
+            error_msg = 'Вы ввели неправильный пароль'
+        form = LoginTeamForm(request.POST)
+        return render(request, auth_page, locals())
+
+
+
+def login(request):
+    form = LoginTeamForm(request.POST)
+    if request.method == 'POST' and form.is_valid():
+        try:
+            team = Teams.objects.get(login=request.POST['login'])
+            if team.password == request.POST['password']:
+                request.session['is_logged'] = True
+                request.session['team_id'] = team.id
+                request.session['team_name'] = team.name
+                request.session['game_id'] = team.game_id
+            else:
+                request.session['404'] = True
+        except Teams.DoesNotExist:
+            request.session['404'] = True
+            pass
+    return HttpResponseRedirect('/')
+
+def logout(request):
+    try:
+        session_keys = list(request.session.keys())
+        for key in session_keys:
+            del request.session[key]
+    except KeyError:
+        pass
+    return HttpResponseRedirect('/')
+
+
 
